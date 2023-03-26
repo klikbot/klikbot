d25ff1a93b10eeb2f373bd1f342532a669860fc4
Elias Biondo <contato@eliasbiondo.com>
2023-03-25 23:12:54 -0300

    chore: update project info

--- src/constants/en/success.ts
+++ src/constants/en/success.ts
@@ -4,35 +4,5 @@ export const success = {
 		code: 1000,
 		title: "API info got successfully.",
 		description: "The API info was got successfully."
-	},
-	deviceCreatedSuccessfully: {
-		status: 201,
-		code: 1201,
-		title: "Device created successfully.",
-		description: "The device was created successfully."
-	},
-	devicesGotSuccessfully: {
-		status: 200,
-		code: 1202,
-		title: "Devices got successfully.",
-		description: "The devices were got successfully."
-	},
-	deviceGotSuccessfully: {
-		status: 200,
-		code: 1203,
-		title: "Device got successfully.",
-		description: "The device was got successfully."
-	},
-	deviceUpdatedSuccessfully: {
-		status: 200,
-		code: 1204,
-		title: "Device updated successfully.",
-		description: "The device was updated successfully."
-	},
-	deviceDeletedSuccessfully: {
-		status: 200,
-		code: 1205,
-		title: "Device deleted successfully.",
-		description: "The device was deleted successfully."
 	}
 };
